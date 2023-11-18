5aa53761af43727fbda865260f0424e71cb59210
brandie8 <brandie120@gmail.com>
2023-11-19 02:55:54 +0300

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
-
-import react from '@vitejs/plugin-react';
-
 export default {
   plugins: [react()],
   build: {
     rollupOptions: {
       external: ['react', 'react-dom', 'react-icons'],
+      plugins: [
+        {
+          name: 'replace-axios',
+          resolveId(source) {
+            if (source === 'axios') {
+              return source; // resolve 'axios' to itself
+            }
+            return null; 
+          },
+        },
+      ],
     },
   },
 };
